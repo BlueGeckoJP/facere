c65ef09e07554e3d86fdd2975682a4de238fba00
bluegecko <kei09doya@gmail.com>
2025-04-04 12:17:50 +0900

    add TodoItem component and update App.vue layout; include main.css for styling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,46 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import TodoItem from "./components/TodoItem.vue";
+</script>
 
-<template></template>
+<template>
+  <div id="app">
+    <div id="title-container">
+      <p id="today-title">Today's ToDo</p>
+      <div id="subtitle-container">
+        <p>2025/01/01 Wed</p>
+        <p>1 / 1 Task Completed</p>
+      </div>
+    </div>
+    <div id="todo-container">
+      <TodoItem />
+      <TodoItem />
+      <TodoItem />
+    </div>
+  </div>
+</template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#title-container {
+  margin: 1rem;
+}
+
+#subtitle-container {
+  display: flex;
+  p {
+    margin-right: 1rem;
+  }
+}
+
+#today-title {
+  font-size: 2rem;
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+}
+
+#todo-container {
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  margin: 1rem;
+}
+</style>
---
+++ src/components/TodoItem.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup></script>
+
+<template>
+  <div class="todo-item">
+    <input type="checkbox" class="todo-checkbox" />
+    <label for="todo-checkbox">Todo Item</label>
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
